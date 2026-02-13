844db85d885a757b4637fd196a17715f7c0b230b
Marcelo Salloum <marcelosalloum@gmail.com>
2026-02-12 17:51:38 -0800

    include request id in pino-http logs for correlation

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -11,6 +11,7 @@ export const logger = pino({ level: Env.logLevel, transport });
 export const pinoHttpSerializers: { [key: string]: SerializerFn } = {
   req(req) {
     return {
+      id: req.id,
       method: req.method,
       url: req.url,
     };
